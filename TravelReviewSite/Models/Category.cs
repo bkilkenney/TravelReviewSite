d4ac854d8c756652bd25d78c285d9394d9b1d8ad
Brad Kilkenney <bkilkenney@gmail.com>
2016-10-07 12:12:38 -0400

    models and contollers created

---
+++ TravelReviewSite/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+
+
+
+namespace TravelReviewSite.Models
+{
+    public class Category
+    {
+        [Key]           //This is the primary key of the Category table and the foreign key of the Reviews table
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ TravelReviewSite/Models/Review.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TravelReviewSite.Models
+{
+    public class Review
+    {
+        [Key] //Primary Key of the Reviews table
+
+        public int ID { get; set; }
+        public string ReviewTitle { get; set; }
+        public string ReviewContent { get; set; }
+        public DateTime PublishDate { get; set; }
+        
+
+        [ForeignKey("Category")]  //CategoryID will be the FK of this table and the PK of Categories table
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+
+
+    }
+}
